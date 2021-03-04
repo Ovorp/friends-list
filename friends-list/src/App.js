24fb32fcc8a477223fedd78c0a19f70812817158
Nathaniel Shirts <nshirts7@gmail.com>
2021-03-03 22:48:08 -0700

    worked on this code along, and figured out if statements in JSX

--- friends-list/src/App.js
+++ friends-list/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      friends: [],
+      picture: '',
+      name: '',
+    };
+  }
+
+  updatePicture = (val) => {
+    this.setState({
+      picture: val,
+    });
+    console.log(this.state);
+  };
+
+  updateName = (val) => {
+    this.setState({
+      name: val,
+    });
+    console.log(this.state);
+  };
+
+  addFriend = () => {
+    let toAdd = this.state.friends;
+    toAdd.push({ picture: this.state.picture, name: this.state.name });
+
+    this.setState({
+      friends: toAdd,
+      picture: '',
+      name: '',
+    });
+  };
+
+  render() {
+    const friends = this.state.friends.map((val) => {
+      return (
+        <div key={'picture'}>
+          <img src={val.picture} alt={`friend`} width="100px"></img>
+          <span>{val.name}</span>
+        </div>
+      );
+    });
+    return (
+      <div onKeyPress={(e) => (e.key === 'Enter' ? this.addFriend() : null)}>
+        <span>Picture: </span>
+        <input
+          onChange={(e) => this.updatePicture(e.target.value)}
+          value={this.state.picture}
+        />
+        <span>Name: </span>
+        <input
+          onChange={(e) => this.updateName(e.target.value)}
+          value={this.state.name}
+        />
+        <button onClick={this.addFriend}>Add Friend</button>
+        {friends}
+      </div>
+    );
+  }
 }
-
-export default App;
